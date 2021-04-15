df9b1a9cdd100469ad213a2dd7a6fb2d3da5cb9c
igorek008@inbox.ru <igorek008@inbox.ru>
2021-04-15 04:40:18 +0400

    Создание класса Player (Спасибо CTX)

--- Form1.cs
+++ Form1.cs
@@ -27,50 +27,7 @@ namespace SimulatorWinForm
 
 
 
-        // Параметры игрока.
-        bool life = true;
-        bool onJob = false;
-
-        int hp = 100;
-        int maxHp = 100;
-
-        double food = 100D;
-        int maxFood = 100;
-
-        double water = 100D;
-        int maxWater = 100;
-
         
-        double energy = 100D;
-        int maxEnergy = 100;
-
-        double intoxication = 0D;
-        int maxIntoxication = 100;
-
-        double money = 0D;
-        double debt = 0D;
-
-        // Скиллы игрока.
-        int iq = 80;
-        int power = 1;
-        int driving = 1;
-        int speech = 1;
-        int virus = 1;
-        int ukraine = 1;
-
-
-        // Прокачка игрока.
-        double exp = 0D;
-        int level = 0;
-        int point = 12;
-
-        // Одежда игрока.
-        int hat = 0;
-        int suit = 0;
-        int boots = 0;
-        int glasses = 0;
-        int mask = 0;
-        int accessory = 0;
 
         // Состояние машины.
         double wheels = 60D;
@@ -81,17 +38,18 @@ namespace SimulatorWinForm
         double gas = 60D;
 
 
-        WindowsMediaPlayer player = new WindowsMediaPlayer();
-        
-       
+        //WindowsMediaPlayer player2 = new WindowsMediaPlayer();
 
 
+
+        Player player1;
 
         public Form1()
         {
             InitializeComponent();
+            player1 = new Player();
             // player.URL = "Ninurta.mp3";
-            
+
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -101,11 +59,11 @@ namespace SimulatorWinForm
 
         public void ListBoxAdd()
         {
-            if (food <= 0 || water <= 0 || energy <= 0)
+            if (player1.food <= 0 || player1.water <= 0 || player1.energy <= 0)
             {
                 listBox1.Items.Insert(0, "Здоровью нанесён урон!");
             }
-            if (exp > 9999)
+            if (player1.exp > 9999)
             {
                 listBox1.Items.Insert(0, "Уровень повышен! Добавлено очко!");
             }
@@ -114,19 +72,19 @@ namespace SimulatorWinForm
         private void timer1_Tick(object sender, EventArgs e)       // Таймер 10 мс.
         {
             TimeLogic();
-            ParametersLogic();
+            player1.ParametersLogic(milSec);
             LabelUpdate();
             LabeColorUpdate();
 
 
             // Настройка прокачки.
-            if (exp > 9999)
+            if (player1.exp > 9999)
             {
-                exp -= 10000;
-                level++;
-                point++;
+                player1.exp -= 10000;
+                player1.level++;
+                player1.point++;
             }
-            progressBar1.Value = (int)exp;
+            progressBar1.Value = (int)player1.exp;
             
 
 
@@ -176,116 +134,47 @@ namespace SimulatorWinForm
 
         }
         
-        private void ParametersLogic()
-        {
-            if (milSec > 60)
-            {
-
-                food -= 0.1;
-                water -= 0.2;
-                intoxication -= 0.4;
-                if (onJob)
-                {
-                    energy -= 0.3;
-                }
-                else
-                {
-                    energy += 0.3;
-                }
-
-            }
-            if (hp > maxHp)         // hp
-            {
-                hp = maxHp;
-            }
-            if (hp < 0)
-            {
-                hp = 0;
-            }
-
-            if (food > maxFood)     // food
-            {
-                food = maxFood;
-            }
-            if (food < 0)
-            {
-                food = 0;
-            }
-
-            if (water > maxWater)   // water
-            {
-                water = maxWater;
-            }
-            if (water < 0)
-            {
-                water = 0;
-            }
-
-            if (energy > maxEnergy) // energy
-            {
-                energy = maxEnergy;
-            }
-            if (energy < 0)
-            {
-                energy = 0;
-            }
-
-            if (intoxication > maxIntoxication) // intoxication
-            {
-                intoxication = maxIntoxication;
-            }
-            if (intoxication < 0)
-            {
-                intoxication = 0;
-            }
-            if (!life)
-            {
-                labelDeath.Visible = true;
-                labelDeath.Text = ($"{labelNick}, ты умер!");
-                tabControl1.Enabled = false;
-
-            }
-        }
+       
         public void LabelUpdate()
         {
             // Обновление параметров игрока.
-            labelHp.Text = ($"hp:{hp}%");
-            labelMaxHp.Text = ($"Max Hp: {maxHp}%");
+            labelHp.Text = ($"hp:{player1.hp}%");
+            labelMaxHp.Text = ($"Max Hp: {player1.maxHp}%");
 
-            Double f = Math.Round(food, 1);
+            Double f = Math.Round(player1.food , 1);
             labelFood.Text = ($"Еда:{f}%");
-            labelMaxFood.Text = ($"Макс. Еды: {maxFood}%");
+            labelMaxFood.Text = ($"Макс. Еды: {player1.maxFood}%");
 
-            Double w = Math.Round(water, 1);
+            Double w = Math.Round(player1.water, 1);
             labelWater.Text = ($"Вода:{w}%");
-            labelMaxWater.Text = ($"Макс. Воды: {maxWater}%");
+            labelMaxWater.Text = ($"Макс. Воды: {player1.maxWater}%");
 
-            Double e = Math.Round(energy, 1);
+            Double e = Math.Round(player1.energy, 1);
             labelEnergy.Text = ($"Энергия:{e}%");
-            labelMaxEnergy.Text = ($"Макс. Энергии: {maxEnergy}%");
+            labelMaxEnergy.Text = ($"Макс. Энергии: {player1.maxEnergy}%");
 
-            Double i = Math.Round(intoxication, 1);
+            Double i = Math.Round(player1.intoxication, 1);
             labelIntoxication.Text = ($"Интоксикация:{i}%");
-            labelMaxIntoxication.Text = ($"Макс. Интоксикации: {maxIntoxication}%");
+            labelMaxIntoxication.Text = ($"Макс. Интоксикации: {player1.maxIntoxication}%");
 
-            Double mo = Math.Round(money, 1);
+            Double mo = Math.Round(player1.money, 1);
             labelMoney.Text = ($"Деньги:{mo} руб.");
-            Double de = Math.Round(debt, 1);
+            Double de = Math.Round(player1.debt, 1);
             linkDebt.Text = ($"Долг: {de} руб.");
 
             // Обновление скиллов игрока.
-            labelIQ.Text = ($"IQ:{iq}");
-            labelPower.Text = ($"Сила:{power}");
-            labelDriving.Text = ($"Вождение:{driving}");
-            labelSpeech.Text = ($"Харизма:{speech}");
-            labelVirus.Text = ($"коронавирусу:{virus}");
-            labelUkraine.Text = ($"Украины:{ukraine}");
+            labelIQ.Text = ($"IQ:{player1.iq}");
+            labelPower.Text = ($"Сила:{player1.power}");
+            labelDriving.Text = ($"Вождение:{player1.driving}");
+            labelSpeech.Text = ($"Харизма:{player1.speech}");
+            labelVirus.Text = ($"коронавирусу:{player1.virus}");
+            labelUkraine.Text = ($"Украины:{player1.ukraine}");
 
             // Обновление прокачки игрока.
-            labelLevel.Text = ($"Уровень: {level}");
-            Double ex = Math.Round(exp, 0);
+            labelLevel.Text = ($"Уровень: {player1.level}");
+            Double ex = Math.Round(player1.exp, 0);
             labelExp.Text = ($"exp: {ex}/10000");
-            labelPoint.Text = ($"Нераспределённых очков:{point}");
+            labelPoint.Text = ($"Нераспределённых очков:{player1.point}");
 
             // Обновление состояния машины.
             Double wh = Math.Round(wheels, 1);
@@ -319,7 +208,7 @@ namespace SimulatorWinForm
                 labelIntoxication.ForeColor = System.Drawing.Color.Black;
             }
 
-            if (onJob)
+            if (player1.onJob)
             {
                 if (milSec > 59.8)
                 {
@@ -341,7 +230,7 @@ namespace SimulatorWinForm
                     labelEnergy.ForeColor = System.Drawing.Color.Black;
                 }
             }
-            if (food <= 0 || water <= 0 || energy <= 0)             //Логическое ИЛИ
+            if (player1.food  <= 0 || player1.water <= 0 || player1.energy <= 0)             //Логическое ИЛИ
             {
                 if (milSec > 59.8)
                 {
@@ -362,7 +251,7 @@ namespace SimulatorWinForm
         {
             groupBox3.Enabled = true;
             groupBox4.Enabled = false;
-            onJob = true;
+            player1.onJob = true;
             button5.Enabled = false;
             button7.Enabled = true;
         }
@@ -371,7 +260,7 @@ namespace SimulatorWinForm
         {
             groupBox3.Enabled = false;
             groupBox4.Enabled = true;
-            onJob = false;
+            player1.onJob = false;
             button5.Enabled = true;
             button7.Enabled = false;
         }
@@ -399,7 +288,7 @@ namespace SimulatorWinForm
             }
             return "Неизвестно";
         }
-        private void button6_Click(object sender, EventArgs e)   // При нажатии кнопки создаётся толпа людей каждый раз
+        private void button6_Click(object sender, EventArgs e)   // При нажатии кнопки создаётся толпа людей каждый раз?
         {
             int r = RandomCustomers();
             customers[0] = new TaxiCustomers("Гордон Фримен", 0, 2);
@@ -425,7 +314,7 @@ namespace SimulatorWinForm
 
 
                 milSec += customers[r].distance / 4;
-                exp += customers[r].distance / 7;
+                player1.exp += customers[r].distance / 7;
                 CarDamage();
                 ListBoxAdd();
                 MoneyManagement();
@@ -458,7 +347,7 @@ namespace SimulatorWinForm
         public void MoneyManagement()
         {
             int r = RandomCustomers();
-            money += customers[r].fare;
+            player1.money += customers[r].fare;
         }
         
         // кнопочки
@@ -477,7 +366,7 @@ namespace SimulatorWinForm
 
         private void button3_Click_1(object sender, EventArgs e)
         {
-            life = false;
+            player1.life = false;
             
 
         }
@@ -493,116 +382,116 @@ namespace SimulatorWinForm
         private void pictureBox8_Click(object sender, EventArgs e)
         {
             SkillUp skillUp = new SkillUp();
-            skillUp.ace = point;
-            skillUp.skill = iq;
+            skillUp.ace = player1.point;
+            skillUp.skill = player1.iq;
             skillUp.SkillManipulated();
-            point = skillUp.ace;
-            iq = skillUp.skill;
+            player1.point = skillUp.ace;
+            player1.iq = skillUp.skill;
         }
 
         private void pictureBox9_Click(object sender, EventArgs e)
         {
             SkillUp skillUp = new SkillUp();
-            skillUp.ace = point;
-            skillUp.skill = power;
+            skillUp.ace = player1.point;
+            skillUp.skill = player1.power;
             skillUp.SkillManipulated();
-            point = skillUp.ace;
-            power = skillUp.skill;
+            player1.point = skillUp.ace;
+            player1.power = skillUp.skill;
         }
 
         private void pictureBox10_Click(object sender, EventArgs e)
         {
             SkillUp skillUp = new SkillUp();
-            skillUp.ace = point;
-            skillUp.skill = driving;
+            skillUp.ace = player1.point;
+            skillUp.skill = player1.driving;
             skillUp.SkillManipulated();
-            point = skillUp.ace;
-            driving = skillUp.skill;
+            player1.point = skillUp.ace;
+            player1.driving = skillUp.skill;
         }
 
         private void pictureBox11_Click(object sender, EventArgs e)
         {
             SkillUp skillUp = new SkillUp();
-            skillUp.ace = point;
-            skillUp.skill = speech;
+            skillUp.ace = player1.point;
+            skillUp.skill = player1.speech;
             skillUp.SkillManipulated();
-            point = skillUp.ace;
-            speech = skillUp.skill;
+            player1.point = skillUp.ace;
+            player1.speech = skillUp.skill;
         }
 
         private void pictureBox13_Click(object sender, EventArgs e)
         {
             SkillUp skillUp = new SkillUp();
-            skillUp.ace = point;
-            skillUp.skill = virus;
+            skillUp.ace = player1.point;
+            skillUp.skill = player1.virus;
             skillUp.SkillManipulated();
-            point = skillUp.ace;
-            virus = skillUp.skill;
+            player1.point = skillUp.ace;
+            player1.virus = skillUp.skill;
         }
 
         private void pictureBox12_Click(object sender, EventArgs e)
         {
             SkillUp skillUp = new SkillUp();
-            skillUp.ace = point;
-            skillUp.skill = ukraine;
+            skillUp.ace = player1.point;
+            skillUp.skill = player1.ukraine;
             skillUp.SkillManipulated();
-            point = skillUp.ace;
-            ukraine = skillUp.skill;
+            player1.point = skillUp.ace;
+            player1.ukraine = skillUp.skill;
         }
 
         private void pictureBox4_Click(object sender, EventArgs e)
         {
             SkillUp skillUp = new SkillUp();
-            skillUp.ace = point;
-            skillUp.skill = maxHp;
+            skillUp.ace = player1.point;
+            skillUp.skill = player1.maxHp;
             skillUp.multiplierSkill = 10;
             skillUp.SkillManipulated();
-            point = skillUp.ace;
-            maxHp = skillUp.skill;
+            player1.point = skillUp.ace;
+            player1.maxHp = skillUp.skill;
         }
 
         private void pictureBox3_Click(object sender, EventArgs e)
         {
             SkillUp skillUp = new SkillUp();
-            skillUp.ace = point;
-            skillUp.skill = maxFood;
+            skillUp.ace = player1.point;
+            skillUp.skill = player1.maxFood;
             skillUp.multiplierSkill = 10;
             skillUp.SkillManipulated();
-            point = skillUp.ace;
-            maxFood = skillUp.skill;
+            player1.point = skillUp.ace;
+            player1.maxFood = skillUp.skill;
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
             SkillUp skillUp = new SkillUp();
-            skillUp.ace = point;
-            skillUp.skill = maxWater;
+            skillUp.ace = player1.point;
+            skillUp.skill = player1.maxWater;
             skillUp.multiplierSkill = 10;
             skillUp.SkillManipulated();
-            point = skillUp.ace;
-            maxWater = skillUp.skill;
+            player1.point = skillUp.ace;
+            player1.maxWater = skillUp.skill;
         }
 
         private void pictureBox6_Click(object sender, EventArgs e)
         {
             SkillUp skillUp = new SkillUp();
-            skillUp.ace = point;
-            skillUp.skill = maxEnergy;
+            skillUp.ace = player1.point;
+            skillUp.skill = player1.maxEnergy;
             skillUp.multiplierSkill = 10;
             skillUp.SkillManipulated();
-            point = skillUp.ace;
-            maxEnergy = skillUp.skill;
+            player1.point = skillUp.ace;
+            player1.maxEnergy = skillUp.skill;
         }
 
         private void pictureBox7_Click(object sender, EventArgs e)
         {
             SkillUp skillUp = new SkillUp();
-            skillUp.ace = point;
-            skillUp.skill = maxIntoxication;
+            skillUp.ace = player1.point;
+            skillUp.skill = player1.maxIntoxication;
             skillUp.multiplierSkill = 10;
             skillUp.SkillManipulated();
-            point = skillUp.ace;
-            maxIntoxication = skillUp.skill;
+            player1.point = skillUp.ace;
+            player1.maxIntoxication = skillUp.skill;
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -656,7 +545,7 @@ namespace SimulatorWinForm
             string[] rumor2 =
             {
                 $"Тут ходят слухи что ", $"Кстати мне вчера {nickName} рассказал что ", $"Я тебе по секрету скажу кое что ", "Слышал чё вчера было тут? Пиздец кароче ",
-                $"Охуеть да у тебя на руках целых {money} рублей)) ебать ты лох)) но щас не об этом. "
+                $"Охуеть да у тебя на руках целых {player1.money} рублей)) ебать ты лох)) но щас не об этом. "
             };
             int rum2 = rnd.Next(rumor2.Length);
 
---
+++ Player.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulatorWinForm
+{
+    class Player
+    {
+        //1. Поля
+
+        // Параметры игрока.
+        public bool life = true;
+        public bool onJob = false;
+
+        public int hp = 100;
+        public int maxHp = 100;
+
+        public double food;
+        public int maxFood = 100;
+
+        public double water = 100D;
+        public int maxWater = 100;
+
+
+        public double energy = 100D;
+        public int maxEnergy = 100;
+
+        public double intoxication = 0D;
+        public int maxIntoxication = 100;
+
+        public double money = 0D;
+        public double debt = 0D;
+
+        // Скиллы игрока.
+        public int iq = 80;
+        public int power = 1;
+        public int driving = 1;
+        public int speech = 1;
+        public int virus = 1;
+        public int ukraine = 1;
+
+
+        // Прокачка игрока.
+        public double exp = 0D;
+        public int level = 0;
+        public int point = 12;
+
+        // Одежда игрока.
+        public int hat = 0;
+        public int suit = 0;
+        public int boots = 0;
+        public int glasses = 0;
+        public int mask = 0;
+        public int accessory = 0;
+
+        //2. Конструтор
+        public Player()
+        {
+            life = true;
+            onJob = false;
+
+            hp = 100;
+            maxHp = 100;
+
+            food = 100D;
+            maxFood = 100;
+
+            water = 100D;
+            maxWater = 100;
+
+
+            energy = 100D;
+            maxEnergy = 100;
+
+            intoxication = 0D;
+            maxIntoxication = 100;
+
+            money = 0D;
+            debt = 0D;
+
+        // Скиллы игрока.
+            iq = 80;
+            power = 1;
+            driving = 1;
+            speech = 1;
+            virus = 1;
+            ukraine = 1;
+
+
+        // Прокачка игрока.
+            exp = 0D;
+            level = 0;
+            point = 12;
+
+        // Одежда игрока.
+            hat = 0;
+            suit = 0;
+            boots = 0;
+            glasses = 0;
+            mask = 0;
+            accessory = 0;
+    
+        }
+        //3. Метод
+        public void ParametersLogic(Double milSec)
+        {
+            if (milSec > 60)
+            {
+
+                food -= 0.1;
+                water -= 0.2;
+                intoxication -= 0.4;
+                if (onJob)
+                {
+                    energy -= 0.3;
+                }
+                else
+                {
+                    energy += 0.3;
+                }
+
+            }
+            if (hp > maxHp)         // hp
+            {
+                hp = maxHp;
+            }
+            if (hp < 0)
+            {
+                hp = 0;
+            }
+
+            if (food > maxFood)     // food
+            {
+                food = maxFood;
+            }
+            if (food < 0)
+            {
+                food = 0;
+            }
+
+            if (water > maxWater)   // water
+            {
+                water = maxWater;
+            }
+            if (water < 0)
+            {
+                water = 0;
+            }
+
+            if (energy > maxEnergy) // energy
+            {
+                energy = maxEnergy;
+            }
+            if (energy < 0)
+            {
+                energy = 0;
+            }
+
+            if (intoxication > maxIntoxication) //intoxication
+            {
+                intoxication = maxIntoxication;
+            }
+            if (intoxication < 0)
+            {
+                intoxication = 0;
+            }
+            /*
+            if (!life)
+            {
+                labelDeath.Visible = true;
+                labelDeath.Text = ($"{labelNick}, ты умер!");
+                tabControl1.Enabled = false;
+
+            }
+            */
+        }
+    }
+}
